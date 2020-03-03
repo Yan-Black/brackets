18700d22400c6b9b957618bf5695ac598e7d11ab
Yan Andryshkin <yan.7feb@gmail.com>
2020-03-03 14:30:32 +0300

    feat: brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let arr = bracketsConfig.map(el => el.join(''))
+
+    for(let i = 0; i < str.length; i++) {
+        if(str.indexOf(arr[i]) != -1) {str = str.replace(arr[i],''); i = arr.indexOf(arr[i] - 1)}
+    }
+  
+  return str.length == 0
 }
